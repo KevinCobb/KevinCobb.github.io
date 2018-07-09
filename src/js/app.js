9775bbdf00645fdde0dff0f40c1e542577a62020
KevinCobb <bender953@gmail.com>
2018-07-08 23:20:10 -0400

    final portfolio

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector('.menu-button')
-
-navToggle.addEventListener('click', _ => {
-document.body.classList.add('nav-is-open')
-
-})
---
+++ src/main/resources/src/js/contact.js
@@ -0,0 +1,7 @@
+const showButton = document.getElementById('showButton');
+showButton.addEventListener('click',function(){
+});
+
+const showButton2 = document.getElementById('showButton2');
+showButton2.addEventListener('click',function(){
+});
